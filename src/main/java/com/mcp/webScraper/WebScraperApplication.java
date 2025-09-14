4c281a397fb3b1d57b3e4d267f53418a990e2651
Rounak Gh <ronakgh97@gmail.com>
2025-09-14 20:13:46 +0530

    Initial commit

---
+++ src/main/java/com/mcp/webScraper/WebScraperApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.webScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperApplication.class, args);
+	}
+
+}
